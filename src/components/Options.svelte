53f8d87d6e3918bf7d052b0122f7beea6b0ddd8f
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2024-02-14 17:31:49 +0100

    options as component

---
+++ src/components/Options.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const options = ['yes', 'unsure', 'no'];
+
+	export let value: string = '';
+
+	const dispatch = createEventDispatcher();
+	const select = (option: string) => () => {
+		value = value == option ? '' : option;
+		dispatch('submit');
+	};
+</script>
+
+<div class="options">
+	{#each options as option}
+		<button aria-current={value === option} on:click={select(option)}>{option.toUpperCase()}</button
+		>
+	{/each}
+</div>
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import answers from '$lib/store.js';
 	import { page } from '$app/stores';
+	import Options from '../../components/Options.svelte';
 
 	export let data;
 
@@ -8,34 +9,21 @@
 
 	let answer: string = $answers[slug];
 
-	// function yes() {
-	// 	answer = 'yes';
-	// }
-	// function no() {
-	// 	answer = 'no';
-	// }
-
 	// This reactive statement udpates when answer changes
 	$: answers.update((answers) => {
 		answers[slug] = answer;
 		return answers;
 	});
+
+	const next = () => {
+		console.log('Go to next page');
+	};
 </script>
 
 <h1>{data.question.title}</h1>
 <div>{@html data.question.content}</div>
 
-{#each ['yes', 'unsure', 'no'] as option}
-	<label>
-		<input type="radio" name="answer" value={option} bind:group={answer} />
-		{option}
-	</label>
-{/each}
-
-<!-- <label>
-	<button on:click={yes}> Yes </button>
-	<button on:click={no}> No </button>
-</label> -->
+<Options bind:value={answer} on:submit={next} />
 
 <p>Current answer: {answer}</p>
 
