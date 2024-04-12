71b81ff0126c7ca910df864954e2532e179620b4
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2024-04-12 16:13:51 +0200

    Improve OrganiserOverview layout

--- src/lib/components/OrganiserOverview.svelte
+++ src/lib/components/OrganiserOverview.svelte
@@ -22,18 +22,22 @@
 							<a
 								style="text-decoration:none; color: #373c44"
 								href="{base}/organiser/{theme.slug}/{question.slug}"
-								><div style="display: flex; margin: 0.5rem">
+								><div
+									style="
+										display: flex; 
+										margin: 0.1rem; 
+										padding-top: 0.2rem; 
+										padding-left: 0.25rem; 
+										border-radius: 0.25rem; 
+										border-width: 0.0625rem;
+  										border-color: rgba(24, 28, 37, 0.5);
+  										border-style:{currentQuestionSlug == question.slug ? 'solid' : undefined};
+										"
+								>
 									<div style="width: 1.5rem">
 										<Indicator audience="organiser" questionSlug={question.slug} />
 									</div>
-									<div
-										style="width: 7rem; font-size:smaller; font-weight:{currentQuestionSlug ==
-										question.slug
-											? 650
-											: undefined}; color:{currentQuestionSlug == question.slug
-											? '#047878'
-											: undefined}"
-									>
+									<div style="width: 8.2rem; font-size:smaller">
 										{question.title}
 									</div>
 								</div></a
@@ -52,8 +56,8 @@
 		margin-bottom: 2rem;
 		display: grid;
 		grid-template-columns: repeat(2, 1fr);
-		grid-column-gap: 0.5rem;
-		grid-row-gap: 0.5rem;
+		grid-column-gap: 0.3rem;
+		grid-row-gap: 0.3rem;
 	}
 	@media screen and (max-width: 768px) {
 		.chapter-overview-grid-container {
@@ -64,8 +68,8 @@
 	.theme-overview-grid-container {
 		display: grid;
 		grid-template-columns: repeat(2, 1fr);
-		grid-column-gap: 0.5rem;
-		grid-row-gap: 0.5rem;
+		grid-column-gap: 0rem;
+		grid-row-gap: 0.3rem;
 	}
 	@media screen and (max-width: 768px) {
 		.theme-overview-grid-container {
